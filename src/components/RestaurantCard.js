11668e761d727f322d9767830d6fb505e3cf32bd
ankit13819 <ankit1381997@gmail.com>
2025-07-09 14:46:36 +0530

    completed 6th lecture

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,15 +2,16 @@ import { styleCard } from "../utils/constants";
 
 const RestrauntCard = (props) => {
   const { resData } = props;
-  // const { name, cuisines, avgRating, cloudinaryImageId } = resData
+  const { name, cuisines, avgRating, cloudinaryImageId, sla, costForTwo } = resData
 
   return (
     <div className='res-card' style={styleCard}>
-      <img className='res-logo' src={`https://media-assets.swiggy.com/swiggy/image/upload/${resData?.cloudinaryImageId}`} alt='res-logo' />
-      <h3>{resData?.name}</h3>
-      <h4>{resData?.cuisines}</h4>
-      <h4>{resData?.avgRating}</h4>
-      <h4>deliveryTime</h4>
+      <img className='res-logo' src={`https://media-assets.swiggy.com/swiggy/image/upload/${cloudinaryImageId}`} alt='res-logo' />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(', ')}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.slaString}</h4>
     </div>
   )
 }
